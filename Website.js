14aa78bd96078595e95f77a40ac74308336b4f6f
Siebe <62479139+SiebeP@users.noreply.github.com>
2020-05-21 15:31:22 +0200

    Update Website.js

--- Website.js
+++ Website.js
@@ -9,3 +9,12 @@ function GoToEindwerk()
   alert("werkt ook")
     window.location = 'Eindwerk.html';
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
