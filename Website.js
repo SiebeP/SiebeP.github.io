63399b6f59eef3e61c3f0e4f9631bcbd346feafd
Siebe <62479139+SiebeP@users.noreply.github.com>
2020-06-09 16:18:34 +0200

    update

--- Website.js
+++ Website.js
@@ -1,17 +1,19 @@
-const sectionsArray = document.querySelectorAll('section');
-const sectionPos = {};
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-sectionsArray.forEach((section) => {
- sectionPos[section.id] = section.offsetTop;
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-window.onscroll = () => {
- var scrollPosition =
-  document.documentElement.scrollTop || document.body.scrollTop;
- for (id in sectionPos) {
-  if (sectionPos[id] <= scrollPosition) {
-   document.querySelector('.active').classList.remove('active');
-   document.querySelector(`a[href*=${id}]`).classList.add('active');
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
- }
-};
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
