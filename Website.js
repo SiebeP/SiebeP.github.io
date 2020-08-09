ed334c42a95e8e5952ba42fbe56e7debf3f0389d
Siebe <62479139+SiebeP@users.noreply.github.com>
2020-08-09 17:39:25 +0200

    makeover 1.0
    totale makeover, navbar verbeterd, foto's in map klueren veranderd

--- Website.js
+++ Website.js
@@ -1,29 +1,40 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+// Wanneer de gebruiker scrolt, start myFunction
+window.onscroll = function()
+{
+	myFunction()
+};
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// Krijg de navbar
+var navbar = document.getElementById("navbar");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// Krijg de offset positie  van de navbar
+var sticky = navbar.offsetTop;
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// Geef de navbar de sticky class wanneer je de scrollpositie berijkt, a verwijder de "sticky" wanner je de scrollpositie verlaat
+function myFuntion()
+	{
+		if (window.pageYOffset >= sticky)
+			{
+				navbar.classList.add("sticky")
+			}
+		else
+			{
+				navbar.classList.remove("sticky")
+			}
+	}
+
 
 //fas-Fa-bars
-function fasFa() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function fasFa()
+{
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav")
+		{
+			x.className += " responsive";
+	  	}
+	else
+		{
+			x.className = "topnav";
+  	}
 }
+
